d587556adfc5f9f986147651ec704dba323d596e
Wojtek Snarski <wsnarski@carthage.edu>
2015-02-21 17:02:45 -0600

    Added view for paths, ability to select path from path view

--- public/javascripts/dagchat_client.js
+++ public/javascripts/dagchat_client.js
@@ -146,11 +146,48 @@ function chatController ($scope, chatSocket, dagPathBuilder, hotkeys) {
     this.clearSelected();
   }
 
-  $scope.showSelectedStory = function() {
+  $scope.changeCompositePath = function(compositePathIndex) {
+    var previousComposite = $scope.selectedCompositePathKey;
+    var previousUnique = $scope.selectedUniquePathKey;
+
+    $scope.selectedCompositePathKey = compositePathIndex;
+    $scope.selectedUniquePathKey = 0;
+    $scope.showSelectedStory({
+      previousComposite: previousComposite,
+      previousUnique: previousUnique
+    });
+  };
+  $scope.changeUniquePath = function(uniquePathIndex) {
+    var previousComposite = $scope.selectedCompositePathKey;
+    var previousUnique = $scope.selectedUniquePathKey;
+
+    $scope.selectedUniquePathKey = uniquePathIndex;
+    $scope.showSelectedStory({
+      previousComposite: previousComposite,
+      previousUnique: previousUnique
+    });
+  };
+  $scope.showSelectedStory = function(previousStory) {
+    //TODO select and deseselect paths so that we can see them
+    if(previousStory !== undefined) {
+      delete $scope.uniquePaths[Object.keys($scope.uniquePaths)
+      [previousStory.previousComposite]].active;
+
+      delete $scope.uniquePaths[Object.keys($scope.uniquePaths)
+      [previousStory.previousComposite]]
+      [previousStory.previousUnique].active;
+    }
     $scope.selectedPath =
       $scope.uniquePaths[Object.keys($scope.uniquePaths)
       [$scope.selectedCompositePathKey]]
       [$scope.selectedUniquePathKey];
+
+    $scope.uniquePaths[Object.keys($scope.uniquePaths)
+      [$scope.selectedCompositePathKey]].active = "active";
+
+    $scope.uniquePaths[Object.keys($scope.uniquePaths)
+      [$scope.selectedCompositePathKey]]
+      [$scope.selectedUniquePathKey].active = "active";
 
     $scope.selectedMessages = [];
     for(var i = 0; i < $scope.selectedPath.length; i++) {
@@ -160,24 +197,32 @@ function chatController ($scope, chatSocket, dagPathBuilder, hotkeys) {
   };
 
   $scope.goToPreviousStory = function() {
+    var previousComposite = $scope.selectedCompositePathKey;
+    var previousUnique = $scope.selectedUniquePathKey;
+    var keys = Object.keys($scope.uniquePaths);
     if($scope.selectedUniquePathKey > 0) {
         $scope.selectedUniquePathKey --;
-      }
-      else if ($scope.selectedCompositePathKey > 0)
-      {
+    }
+    else if ($scope.selectedCompositePathKey > 0){
         $scope.selectedCompositePathKey --;
-      }
-      else {
-        var keys = Object.keys($scope.uniquePaths);
+        $scope.selectedUniquePathKey =
+        $scope.uniquePaths[keys[$scope.selectedCompositePathKey]].length - 1;
+    }
+    else {
         $scope.selectedCompositePathKey = keys.length - 1;
         $scope.selectedUniquePathKey =
           $scope.uniquePaths[keys[keys.length-1]].length - 1;
-      }
+    }
 
-      $scope.showSelectedStory();
+    $scope.showSelectedStory({
+      previousComposite: previousComposite,
+      previousUnique: previousUnique
+    });
   };
 
   $scope.goToNextStory = function() {
+    var previousComposite = $scope.selectedCompositePathKey;
+    var previousUnique = $scope.selectedUniquePathKey;
     if($scope.selectedUniquePathKey <
       $scope.uniquePaths[Object.keys($scope.uniquePaths)
       [$scope.selectedCompositePathKey]].length - 1) {
@@ -187,12 +232,16 @@ function chatController ($scope, chatSocket, dagPathBuilder, hotkeys) {
               Object.keys($scope.uniquePaths).length -1 )
      {
        $scope.selectedCompositePathKey ++;
+       $scope.selectedUniquePathKey = 0;
      }
      else {
        $scope.selectedUniquePathKey = 0;
        $scope.selectedCompositePathKey = 0;
      }
 
-     $scope.showSelectedStory();
+     $scope.showSelectedStory({
+       previousComposite: previousComposite,
+       previousUnique: previousUnique
+     });
   };
 }]);
